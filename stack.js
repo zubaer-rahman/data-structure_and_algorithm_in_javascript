4cc15a0f2bd125a329029376e1ef3e285406ea4a
zubaer-rahman <zubaer.rahman.cse@gmail.com>
2022-09-19 11:10:51 +0600

    satck is done

---
+++ stack.js
@@ -0,0 +1,26 @@
+class Stack {
+    constructor(){
+        this.stack = [];
+    }
+    add(item) {
+        this.stack.push(item);
+    }
+    remove() {
+        if (this.stack.length) {
+            return this.stack.pop();
+        }
+    }
+}
+const guest = new Stack();
+guest.add("Sufi");
+guest.add("Nayon");
+guest.add("Chayan");
+guest.add("Zubaer");
+
+console.log(guest.stack);
+
+const speaker = guest.remove();
+
+console.log(speaker);
+
+console.log(guest.stack);
